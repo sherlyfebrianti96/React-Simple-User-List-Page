1134c76d017b09487bb4d1e30d0d51a13448b093
Sherly Febrianti <sherly.febrianti@allianz.co.id>
2020-08-18 01:06:56 +0700

    Include the Build result

---
+++ build/precache-manifest.0d1acf5653657c6de3cd1145792684ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81d81740129afe0682a778b9d636441",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0d8f712e457aa737a5",
+    "url": "/static/css/main.74f9d43f.chunk.css"
+  },
+  {
+    "revision": "10b4846a61d207353c0e",
+    "url": "/static/js/2.a45c7d3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a45c7d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d0d8f712e457aa737a5",
+    "url": "/static/js/main.2e6eb9ee.chunk.js"
+  },
+  {
+    "revision": "0473aaa3b1845e610211",
+    "url": "/static/js/runtime-main.61c162a0.js"
+  }
+]);
